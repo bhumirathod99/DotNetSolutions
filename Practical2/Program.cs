9e9d0cd3886aa9c278e408eb2af51b1a21f70bf0
Rathod Bhumi <rathodbhumi199@gmail.com>
2020-01-21 00:23:34 -0800

    Initial commit

---
+++ Practical2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practical2
+{
+    class Program
+    {
+        static void rectangle()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        static void tringle_left()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        static void tringle_right()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.Write("\n");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            rectangle();
+            Console.WriteLine();
+            tringle_left();
+            Console.Read();
+        }
+    }
+}
