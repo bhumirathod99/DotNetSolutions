b9478a9dc0621a762a1ec5a1d27b474e4ba80fee
bhumirathod99 <rathodbhumi199@gmail.com>
2020-06-01 20:07:18 +0530

    initial commit

--- Practical2/Program.cs
+++ Practical2/Program.cs
@@ -1,63 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+
+using System.Text.RegularExpressions;
 
 namespace Practical2
+
 {
+
     class Program
+
     {
-      
-        static void rectangle()
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    Console.Write("*");
-                }
-                Console.Write("\n");
-            }
-        }
-
-        static void tringle_left()
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j <= i; j++)
-                {
-                    Console.Write("*");
-                }
-                Console.Write("\n");
-            }
-        }
-
-        static void tringle_right()
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = i; j < 5; j++)
-                {
-                    Console.Write(" ");
-                }
-                for (int j = 0; j <= i; j++)
-                {
-                    Console.Write("*");
-                }
-                Console.Write("\n");
-            }
-        }
-
 
         static void Main(string[] args)
+
         {
-           
-            rectangle();
-            Console.WriteLine();
-            tringle_left();
-            Console.WriteLine();
-            tringle_right();
+
+            string name, country;
+
+            do
+
+            {
+
+                Console.WriteLine("Hello! Whats your name?");
+
+                name = Console.ReadLine();
+
+            } while (!Regex.IsMatch(name,@"^[a-zA-Z]+$"));
+
+            do
+
+            {
+
+                Console.WriteLine("Where are you from?");
+
+                country = Console.ReadLine();
+
+            } while (!Regex.IsMatch(country,@"^[a-zA-Z]+$"));
+
+
+
+            Console.WriteLine($"Hello {name} from country {country}");
+
             Console.Read();
+
         }
+
     }
+
 }
